438c5b1f72778efb550aa033648846fb17ca869b
Jon Ascencio <jonascencio1@gmail.com>
2020-10-27 18:53:44 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv'); // has our config/variables
+/* const morgan = require('morgan'); // used so taht when there is any request, it'll show in the console
+ */
+// to load config file
+dotenv.config( { path: './config/config.env' } ); // where we place our global variables. port also goes here. can also store mongo URI
+
+
+const app = express(); // initialzie app with express 
+
+// logging 
+/* if (process.env.NODE_ENV === 'development') {
+  // add morgan middleware
+  app.use(morgan('dev')); // shows http method and res in console 
+} */
+
+// whenever we use process.env we can use variables in that config. if not there then we use port 5000
+const PORT = process.env.PORT || 5000; 
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)); 
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  // our react entry file that inside a source file
+  entry: path.resolve(__dirname, './client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'), //it will be placed in this build folder
+    filename: 'bundle.js'
+  }, 
+  mode: process.env.NODE_ENV,
+  module: { // all the modules/files within a project and  specifies how each file is going to be treated that we come across via loaders 
+    rules: [ // rules consist of three things: condition (which type of file transformation is applied to),  result and there might be nested rules too
+      {
+      test: /\.jsx?/, // we can tell which file we are choosing via tests
+      exclude: /(node_modules)/, // we exclude going through node modules because we should already assume that they should be in a format that is workable and thus don't need transpiling 
+      use: { // referred to as a rule result and is used when a rule condition matches. ie we are going to "use" a loader 
+        loader: 'babel-loader', 
+        options: { // then we specify options for each line of file
+          presets: ['@babel/preset-env', '@babel/preset-react'] // in a babel a preset allows us to work with any and all jsx/react files. within this environemnt we are only going to be interested in jsx files. but preset-env transpiles all es6 code and the second file tells it to only work on react code
+        }
+      }
+      }, 
+      {
+        test:  /\.s[ac]ss$/i, // same thing but will run these rules on any file that ends with s[ac]ss. can also use /\.s?css$/ to track anything with scss or css files 
+        exclude: /(node_modules)/,
+        use:  [
+          // creates style nodes from JS strings
+          'style-loader',
+        
+          // transolates CSS into commonJS
+          'css-loader',
+          // compiles Sass to CSS
+          'sass-loader'
+        ] // this array is done in reverse order . can also use style!css!sass. ie is read from right to left 
+      },
+    ]
+  }, 
+}
